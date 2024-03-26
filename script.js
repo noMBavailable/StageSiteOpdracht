723556144899311107ab0008bd39660ec7ffe157
Marc Baudet <marcbaudet@live.nl>
2024-03-26 13:33:11 +0100

    added js

---
+++ script.js
@@ -0,0 +1,22 @@
+//make multi const images for the different projects
+
+const images = ['images/FTCMenu.PNG', 'images/stockphoto12.jpg', 'images/notebook-rotated.png', 'images/mathias-reding-P0YeIVOyvSI-unsplash.jpg'];
+let currentIndex = 0;
+
+function prevImage() {
+  if (currentIndex === 0) {
+    currentIndex = images.length - 1;
+  } else {
+    currentIndex--;
+  }
+  document.getElementById('slider-image').src = images[currentIndex];
+}
+
+function nextImage() {
+  if (currentIndex === images.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  document.getElementById('slider-image').src = images[currentIndex];
+}
