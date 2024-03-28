358411f2833ee06b6f37349379fa6b344ef56ba4
Marc Baudet <marcbaudet@live.nl>
2024-03-28 11:03:08 +0100

    safepoint: making projects folder connected with buttons and multiple images

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 //make multi const images for the different projects
 
 const images = ['images/FTCMenu.PNG', 'images/stockphoto12.jpg', 'images/notebook-rotated.png', 'images/mathias-reding-P0YeIVOyvSI-unsplash.jpg'];
-let currentIndex = 0;
+let currentIndexImg = 0;
 
 function prevImage() {
-  if (currentIndex === 0) {
-    currentIndex = images.length - 1;
+  if (currentIndexImg === 0) {
+    currentIndexImg = images.length - 1;
   } else {
-    currentIndex--;
+    currentIndexImg--;
   }
-  document.getElementById('bannerImage').src = images[currentIndex];
+  document.getElementById('bannerImage').src = images[currentIndexImg];
 }
 
 function nextImage() {
-  if (currentIndex === images.length - 1) {
-    currentIndex = 0;
+  if (currentIndexImg === images.length - 1) {
+    currentIndexImg = 0;
   } else {
-    currentIndex++;
+    currentIndexImg++;
   }
-  document.getElementById('bannerImage').src = images[currentIndex];
+  document.getElementById('bannerImage').src = images[currentIndexImg];
 }
+
+
+const imageArray = [];
+
+imageArray[0] = 'images/FTCMenu.PNG';
+imageArray[1] = 'images/stockphoto12.jpg';
+imageArray[2] = 'images/notebook-rotated.png';
+imageArray[3] = 'images/mathias-reding-P0YeIVOyvSI-unsplash.jpg';
+
+let currentProjectIndex = 0;
+function changeProject(currentProjectIndex) {
+ imageArray[currentProjectIndex];
+
+console.log("currentProjectIndex: " + currentProjectIndex); 
+console.log("currentImageIndex: " + imageArray[currentProjectIndex]); 
+
+  
+}
+
